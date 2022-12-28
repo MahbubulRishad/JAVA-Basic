6fe5d085a7b01d7574e8c4cf4401ec2ad516e9fb
MahbubulRishad <mahbubulrishad@gmail.com>
2022-12-29 01:42:08 +0600

    Third commit

--- src/main/java/for_each_loop/ForEachLoopTest.java
+++ src/main/java/for_each_loop/ForEachLoopTest.java
@@ -8,6 +8,7 @@ public class ForEachLoopTest {
         }
 
         int[] numbers = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        double[] amount = {10.1, 20.3, 30.5, 40.6, 50.1, 60.00, 70.66, 80.1, 90.6, 100.22};
         String[] names = {"Rishad", "Rifat", "Raka", "Rahat", "Rohan", "Rimon"};
 
 //        printing number using for each loop
@@ -19,5 +20,11 @@ public class ForEachLoopTest {
         for (String str : names) {
             System.out.println("Names are: " + str);
         }
+
+//        Printing double value using For Each Loop
+        for (double am : amount) {
+            System.out.println("Amount is: " + am);
+        }
+
     }
 }
