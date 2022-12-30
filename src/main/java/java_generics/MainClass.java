57096a71decb5cbf719090c843ff47f75c95b0e4
MahbubulRishad <mahbubulrishad@gmail.com>
2022-12-31 00:38:49 +0600

    third commit

--- src/main/java/java_generics/MainClass.java
+++ src/main/java/java_generics/MainClass.java
@@ -13,5 +13,22 @@ public class MainClass {
 //        print String
         PrintString printString = new PrintString("Java Learning");
         System.out.println(printString.getString());
+
+        System.out.println("-----Generic Start-----");
+
+//    Generic Start (Print Integer)
+        GenericStart <Integer> integerGenericStart = new GenericStart<>(50);
+        System.out.print("Integer No: ");
+        System.out.println(integerGenericStart.getObj());
+
+//        Generic Start (Print String)
+        GenericStart <String> stringGenericStart = new GenericStart<>("String from Generic start class");
+        System.out.print("String: ");
+        System.out.println(stringGenericStart.getObj());
+
+//        Generic Start (print Double)
+        GenericStart <Double> doubleGenericStart = new GenericStart<>(56.3);
+        System.out.print("Double No: ");
+        System.out.println(doubleGenericStart.getObj());
     }
 }
