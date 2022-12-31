9ad444ebcb1f62c579734e0cd2ebd5fb3a77dbca
MahbubulRishad <mahbubulrishad@gmail.com>
2023-01-01 00:23:16 +0600

    List Test

--- src/main/java/java_generics/GenericListTest.java
+++ src/main/java/java_generics/GenericListTest.java
@@ -1,4 +1,25 @@
 package java_generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GenericListTest {
+    public static void main(String[] args) {
+
+//        List
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(14);
+        integerList.add(50);
+        integerList.add(21);
+        integerList.add(188);
+        integerList.add(16);
+
+        integerList.remove(2);
+
+        integerList.add(144);
+
+        for (Integer integer : integerList) {
+            System.out.println(integer);
+        }
+    }
 }
